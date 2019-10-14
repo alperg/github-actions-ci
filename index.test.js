83ae32097a6b0a7bb280e4ba3f09830f03c231a2
Alper Gokcehan <mail@alperg.com>
2019-10-14 11:54:28 -0400

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -3,3 +3,8 @@ const weekNum = require('./index.js');
 test('that weekNum returns a value', () => {
   expect(weekNum).not.toBeNull();
 });
+
+// new test added
+test('that weekNum returns a number', () => {
+  expect(weekNum).toBeDefined();
+});
