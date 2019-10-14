eff73a55cf1d9bd13e238167d5966f5e2a7185dd
Node Code Formatter <formatter@1337z.ninja>
2019-10-14 16:04:44 +0000

    Formatted code

--- index.test.js
+++ index.test.js
@@ -1,15 +1,15 @@
-const weekNum = require('./index.js');
+const weekNum = require('./index.js')
 
 test('that weekNum returns a value', () => {
-  expect(weekNum).not.toBeNull();
-});
+  expect(weekNum).not.toBeNull()
+})
 
 // new test added
 test('that weekNum returns a number', () => {
-  expect(weekNum).toBeDefined();
-});
+  expect(weekNum).toBeDefined()
+})
 
 // new test 2 added
 test('that true is true', () => {
-  expect(true).toBe(true);
-});
+  expect(true).toBe(true)
+})
